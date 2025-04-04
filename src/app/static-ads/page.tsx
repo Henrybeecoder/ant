06b2c56f6f7d97451e2c0102f9d4439a6f1432a5
Henry Omofonmwan <omofonmwanhenrynosa@gmail.com>
2025-04-04 09:59:25 +0100

    Revert "This a new push"
    This reverts commit 7ee29db9992624fa978f8725a59c35560cc83e04.

--- src/app/static-ads/page.tsx
+++ src/app/static-ads/page.tsx
@@ -4,14 +4,11 @@ import Image from "next/image"
 import { motion } from 'framer-motion'
 import BrandMarquee from "@/components/brandMarquee/page"
 import StaticAdsSlider from "@/components/static-ads-slider/page"
-import BookCallButtonStaticAds from "@/components/book-call-btn-static-ads/page"
-import FAQSection from "@/components/faq-section/page"
-import TestimoniesStatic from "@/components/testimonies-static-slider/page"
 
 function StaticAds () {
     const brandImages = Array.from({ length: 11 }, (_, i) => i + 1);
     return (
-<AppLayout title={'Static ads'} >
+<AppLayout>
      {/* Hero Section */}
      <motion.div
         className="mt-30 flex relative flex-col justify-center items-center"
@@ -20,7 +17,7 @@ function StaticAds () {
         <div className="lg:w-[65%] w-[100%]">
           <p
         
-            className="font-[500] text-center lg:px-[270px] px-10 lg:text-[2.1rem] lg:leading-9 leading-7 text-[1.25rem]"
+            className="font-[500] text-center lg:px-[230px] px-0 lg:text-[2.1rem] lg:leading-9 leading-7 text-[1.25rem]"
           >
          Statics that sell. 
          Delivered in  {''}
@@ -28,19 +25,27 @@ function StaticAds () {
              24 hours.
             </span>
           </p>
-          <p className="font-[400] my-5 text-center lg:px-[180px] px-0 text-[#FFFFFFB2] lg:text-[1rem] text-[0.875rem]">
+          <p className="font-[400] my-5 text-center lg:px-[160px] px-0 text-[#FFFFFFB2] lg:text-[1rem] text-[0.875rem]">
           No shoots. No shipping. Unlimited requests. Unlimited revisions. 
           Visuals that make your brand feel like a million bucks, without spending it.
           </p>
           <StaticAdsSlider/>
           <div className="w-full flex flex-row cursor-pointer justify-center mt-8">
-          <BookCallButtonStaticAds />
+            <button className="bg-[#4101F6] text-white font-[600] text-[1rem] flex flex-row px-1 py-1 cursor-pointer hover:bg-[#2e00b3] transition-all">
+              <Image src="/assets/images/ose.png" alt="" width={50} height={50} />
+              <div className="text-[#FFFFFF] h-full flex flex-col justify-center items-center mx-3">
+                <p className="font-[600] lg:text-[1rem] text-[0.8rem]">
+                  Book a call with Ose (founder)
+                </p>
+                <p className="font-[500] lg:text-[0.75rem] text-[0.7rem]">
+                  (It’s not going to be a sales pitch)
+                </p>
+              </div>
+            </button>
           </div>
           {/* Brand Slider */}
           {/* @ts-expect-error: Temporarily ignoring type error while refactoring */}
      <BrandMarquee brandImages={brandImages} />
-     <TestimoniesStatic />
-     <FAQSection static />
         
         </div>
       </motion.div>
--- src/components/faq-section/page.tsx
+++ src/components/faq-section/page.tsx
@@ -1,22 +1,17 @@
 import { faqData } from "@/utils/data";
-import { newFaqData } from "@/utils/data";
+
 import FAQItem from "../faq-item/page";
 
-interface FAQSectionProps {
-  static?: boolean;
-}
-
-const FAQSection: React.FC<FAQSectionProps> = ({ static: isStatic }) => {
-  const dataToUse = isStatic ? newFaqData : faqData;
-
+const FAQSection = () => {
   return (
     <div className="mt-40">
       <p className="text-center text-[1.6rem] lg:leading-0 leading-8 mb-4 lg:mb-10">
         Frequently asked questions
       </p>
-      {dataToUse.map((faq, index) => (
+      {faqData.map((faq, index) => (
         <FAQItem key={index} faq={faq} />
       ))}
+     
     </div>
   );
 };
